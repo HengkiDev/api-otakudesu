6d968dcb7a8b24722e8d582e2b46f5d242e36348
Hengki.Dev <hengkiyunqi@yahoo.com>
2025-04-04 02:34:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 const BASE_URL = 'https://otakudesu.cloud';
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
 
+// Disable JSON pretty printing
+app.set('json spaces', 0);
 app.use(cors());
 
 // Helper functions
